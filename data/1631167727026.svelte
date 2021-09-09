d577fe1cde5078f028c1020b4e0795d6f429aaf6
rokkuminchan <71059093+rokkuminchan@users.noreply.github.com>
2021-09-09 15:08:47 +0900

    createOrUpdateFile

---
+++ data/1631167727026.svelte
@@ -0,0 +1,16 @@
+
+<script context='module'>
+  export const load = async function ({ page, fetch, session, context }) {
+    const data = await import('$lib/data/blogs/{blogId}.json');
+
+    return { props: { data: data.default } };
+  };
+</script>
+
+<script>
+  import BlogDetailPageContent from '$lib/components/blogDetail/blogDetailPageContent.svelte';
+
+  export let data;
+</script>
+
+<BlogDetailPageContent {data} />
